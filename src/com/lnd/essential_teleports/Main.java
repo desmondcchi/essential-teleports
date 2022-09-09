ac113d05601536c70d2a325bfddb5c9615ad87a8
BuildTools <unconfigured@null.spigotmc.org>
2022-09-09 03:29:27 -0700

    First plugin success

--- src/com/lnd/essential_teleports/Main.java
+++ src/com/lnd/essential_teleports/Main.java
@@ -2,10 +2,12 @@ package com.lnd.essential_teleports;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.lnd.essential_teleports.commands.Spawn;
+
 public class Main extends JavaPlugin {
 	@Override 
 	public void onEnable() {
-		
+		this.getCommand("spawn").setExecutor(new Spawn());
 	}
 	
 	@Override 
